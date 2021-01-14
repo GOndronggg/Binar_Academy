26b8abad2bbd8145eea36142c8f4d2bc8c3f048e
GOndronggg <andreamarco1212@gmail.com>
2021-01-14 15:30:16 +0700

    update

--- Week 5/Day 4/index.js
+++ Week 5/Day 4/index.js
@@ -1,5 +1,28 @@
+// Init Sequelize
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("database", "username", "password", {
+// Connect Sequelize
+const sequelize = new Sequelize("binaracademy", "mawang", "Mawang1!", {
   host: "localhost",
   dialect: "mariadb",
 });
+
+const testConnection = async () => {
+  // Check Database Connection
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+const getAllStudent = async () => {
+  const data = await sequelize.query(
+    "select * from binaracademy.tbl_student where id = 3"
+  );
+  return data;
+};
+
+testConnection();
+
+getAllStudent().then((data) => console.log(data[0][0].id));
